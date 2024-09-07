ccd682c61ae7ce5f9fc71bd08b17cfd5fb85fe5c
aregawih <35622084+aregawih@users.noreply.github.com>
2024-09-08 02:48:30 +0300

    header and footer

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Compenents/Header/Header'
+import Footer from '../../Compenents/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
