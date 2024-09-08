e0ae9c74968a6a7a7e98a85680cb37032318fd8e
aregawih <35622084+aregawih@users.noreply.github.com>
2024-09-08 20:31:25 +0300

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Compenents/Header/Header'
 import Footer from '../../Compenents/Footer/Footer'
+import Banner from '../../Compenents/Banner/Banner'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   )
