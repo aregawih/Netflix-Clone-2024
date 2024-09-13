4504e8eb918c3a68f4569550fd50aead73b5e34f
aregawih <35622084+aregawih@users.noreply.github.com>
2024-09-13 17:34:41 +0300

    row and deployment

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Header from '../../Compenents/Header/Header'
 import Footer from '../../Compenents/Footer/Footer'
 import Banner from '../../Compenents/Banner/Banner'
+import RowList from '../../Compenents/Rows/RowList/RowList'
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
-  )
+  );
 }
 
 export default Home
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -3,9 +3,11 @@ const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28&api_key=${API_KEY}`,
+  fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with-genres=28`,
+  // fetchActionMovies: `/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28&api_key=${API_KEY}`,
   fetchCommedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with-generes=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
   fetchTvShow: `/tv/popular?language=en-US&page=1&api_key=${API_KEY}`,
 };
